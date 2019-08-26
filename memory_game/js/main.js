04dbd6356a103fad740795204e8df96991f46da5
Matthew Miles <milesm427@gmail.com>
2019-08-26 19:54:57 -0400

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,37 @@
+
+// test main.js linked file with script tags
+// ctrl + shft + j opens the chrome console in windows 10
+console.log('Up and running');
+
+
+// create the 4 cards for our memory game
+
+const cardOne = "queen";
+const cardTwo = "queen";
+const cardThree = "king";
+const cardFour = "king";
+
+
+
+// flipping two cards to test concatenation with string variables.
+
+console.log('User flips ' + cardOne);
+console.log('User flips ' + cardTwo);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   placeholder so I can save without losing working space, ie line breaks.
