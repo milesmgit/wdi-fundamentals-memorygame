dc6005820c665d7870dd39aeb5b418fa17367366
Matthew Miles <milesm427@gmail.com>
2019-08-28 22:22:21 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -24,7 +24,28 @@
 
 
 // cards from which to choose.
-const cards = ['queen', 'queen', 'king', 'king'];
+const cards = [
+  {
+    rank : "queen",
+    suit : "hearts",
+    cardImage : "images/queen-of-hearts.png"
+  },
+  {
+    rank : "queen",
+    suit : "diamonds",
+    cardImage : "images/queen-of-diamonds.png"
+  },
+  {
+    rank : "king",
+    suit : "hearts",
+    cardImage : "images/king-of-hearts.png"
+  },
+  {
+    rank : "king",
+    suit : "diamonds",
+    cardImage : "images/king-of-diamonds.png"
+  }
+];
 
 // cards in play
 const cardsInPlay = [];
@@ -38,6 +59,7 @@ function checkForMatch(){
       console.log('You found a match');
     } else {
         console.log('Sorry, try again');
+        alert('Sorry, try again');
     }
   }
 }
@@ -49,9 +71,12 @@ function checkForMatch(){
 function flipCard(cardId){
 
   // console.log the card that was flipped over
-  console.log('User flipped ' + cards[cardId]);
+  console.log('User flipped ' + cards[cardId].rank);
+  console.log(cards[cardId].cardImage);
+  console.log(cards[cardId].suit);
 
-  cardsInPlay.push(cards[cardId]);
+
+  cardsInPlay.push(cards[cardId].rank);
   checkForMatch();
 
 }
